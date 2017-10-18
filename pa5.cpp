8ba0b6b1e164765e2ccf3375e411f433875d7bad
Alice Easter <alice.easter@mymail.champlain.edu>
2017-10-18 10:14:17 -0400

    All the Comments

--- pa5.cpp
+++ pa5.cpp
@@ -1,3 +1,15 @@
+/* ***       Author:  Adam DeCosta & Alice Easter
+     *  Last Update:  October 18, 2017
+     *        Class:  CSI-281
+     *     Filename:  pa5.cpp
+     *
+     *  Description:
+     *      Stub driver for testing
+     *
+     *  Certification of Authenticity:
+     *     I certify that this assignment is entirely my own work.
+     ********************************************************************/
+
 #include <iostream>
 
 int main()
