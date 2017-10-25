dac8e336b69db3480c7cbb5c0ece1ca77d567d45
Alice Easter <alice.easter@mymail.champlain.edu>
2017-10-25 14:30:15 -0400

    Beginnings of a stub driver

--- pa5.cpp
+++ pa5.cpp
@@ -11,9 +11,40 @@
      ********************************************************************/
 
 #include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+#include <fstream>
+#include <time.h>
+#include <string>
+#include "timerSystem.h"
+#include "linkedList.h"
+#include "DoublyLinkedList.h"
+
+const int SIZE = 1000000;
 
 int main()
 {
-	std::cout << "Hello, WOrld!" << std::endl;
+	std::ifstream inputFile;
+	std::string line;
+	int *arr;
+	arr = new int[SIZE];
+	LinkedList<int> linkedList = LinkedList<int>();
+	DoublyLinkedList<int> doublyLinkedList = DoublyLinkedList<int>();
+
+	
+	int i = 0;
+
+	inputFile.open("output.txt");
+
+	while (!inputFile.eof())
+	{
+		getline(inputFile, line);
+		arr[i] = std::stoi(line);
+		i++;
+	}
+
+
+
+	inputFile.close();
 	return 0;
 }
